cb2d1f72f48bf106e68dd1008138ccdb4b028736
IngJorgeArana005 <ing.jorge.arana.005@gmail.com>
2017-01-14 10:37:27 -0600

    Added the footer

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php $ptitle = "GBATSS | Main"; ?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"].'/inc/head.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/inc/head.php'; ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/inc/footer.php'; ?>
